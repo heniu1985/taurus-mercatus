20e4cb4e829bb22d59eb5fa7268f665309c9428c
heniu1985 <heniu_1@o2.pl>
2020-09-29 20:22:52 +0200

    Practicing with plots

--- test.py
+++ test.py
@@ -1,26 +1,28 @@
+import csv
+
+from datetime import datetime
+
 import matplotlib.pyplot as plt
-import matplotlib.dates as mpl_dates
-
-from mplfinance.original_flavor import candlestick_ohlc
-
-import pandas as pd
 
 filename = "data/mbk_w.csv"
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, closes = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        close = float(row[4])
+        dates.append(current_date)
+        closes.append(close)
 
 plt.style.use("seaborn")
-
-data = pd.read_csv(filename)
-ohlc = data.loc[:, ["Data", "Otwarcie", "Najwyzszy", "Najnizszy", "Zamkniecie"]]
-ohlc["Data"] = pd.to_datetime(ohlc["Data"])
-ohlc["Data"] = ohlc["Data"].apply(mpl_dates.date2num)
-ohlc = ohlc.astype(float)
-
 fig, ax = plt.subplots()
+ax.plot(dates, closes, c="blue")
 
-candlestick_ohlc(ax, ohlc.values, width=0.6, colorup="green", colordown="red", alpha=0.8)
-
-ax.set_xlabel('Data')
-ax.set_ylabel('Cena')
-fig.suptitle('mBank')
+ax.set_title("mBank", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+ax.set_ylabel("Price", fontsize=16)
+ax.tick_params(axis="both", which="major", labelsize=16)
 
 plt.show()
