bb2b39ce78fae2cf20ad5f50da2634feac1be6a3
heniu1985 <heniu_1@o2.pl>
2020-09-30 15:51:47 +0200

    Learning

--- plot_learning.py
+++ plot_learning.py
@@ -6,3 +6,14 @@ import matplotlib.pyplot as plt
 
 filename = "data/mbk_w.csv"
 
+mbank = pd.read_csv(filename)
+mbank = mbank[["Data", "Zamkniecie"]]
+mbank["MA 30"] = mbank.iloc[:, 1].rolling(window=30).mean()
+
+plt.figure(figsize=[15,10])
+plt.grid(True)
+plt.plot(mbank["Zamkniecie"], label="close")
+plt.plot(mbank["MA 30"], label="MA 30")
+plt.legend(loc=2)
+
+plt.show()
