7c3a3eb83207dbdc7c189aae40823dd0c5e98c3a
heniu1985 <heniu_1@o2.pl>
2020-10-09 13:27:52 +0200

    Changes 1. Created taurus.py 2. Change docstrings in indicators.py

--- indicators.py
+++ indicators.py
@@ -1,14 +1,32 @@
+# This file contains technical analisys indicators that I use in my strategies
+
 import pandas as pd
 
 def moving_average(df, periods=30):
-    # Function calculating Moving Average for given data
+    """Function calculating Moving Average (MA) for given data
+
+    Args:
+        df (pandas.DataFrame): Quotes with closing prices
+        periods (int, optional): The number of periods from which MA is calculated. Defaults to 30.
+
+    Returns:
+        pandas.DataFrame: Quotes extended by the calculated MA
+    """
     ma = pd.Series(df['Zamkniecie'].rolling(periods, min_periods=periods).mean(), name='MA_' + str(periods))
     df = df.join(ma)
     return df
 
 
 def exponential_moving_average(df, periods=30):
-    # Function calculating Moving Average for given data
+    """Function calculating Exponential Moving Average (EMA) for given data
+
+    Args:
+        df (pandas.DataFrame): Quotes with closing prices
+        periods (int, optional): The number of periods from which EMA is calculated. Defaults to 30.
+
+    Returns:
+        pandas.DataFrame: Quotes extended by the calculated EMA
+    """
     ema = pd.Series(df['Zamkniecie'].ewm(periods, min_periods=periods).mean(), name='EMA_' + str(periods))
     df = df.join(ema)
     return df
---
+++ taurus.py
@@ -0,0 +1,8 @@
+import csv
+
+import requests
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
