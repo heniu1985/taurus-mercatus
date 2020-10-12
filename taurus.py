79f3b0cb60e40e02418aae484987fa5471a66981
heniu1985 <heniu_1@o2.pl>
2020-10-12 19:20:45 +0200

    Update 12.10 19:20

--- taurus.py
+++ taurus.py
@@ -18,8 +18,12 @@ DOWNLOAD_PATH = "https://static.stooq.pl/db/h/d_pl_txt.zip"
 ARCHIVE_PATH = "data/all_data.zip"
 ZIP_UNPACK_PATH = "data/daily/pl/wse stocks/"
 
-def download_datas():
+# Functions
 
+def download_datas():
+    """Function download data from DOWNLOAD_PATH
+    Data comes from website Stooq - https://stooq.pl/
+    """
     link = requests.get(DOWNLOAD_PATH)
 
     try:
@@ -27,12 +31,13 @@ def download_datas():
     except Exception:
         print("Datas not downloded!!!")
         
-    with open(ARCHIVE_PATH, "wb") as TP:
+    with open(ARCHIVE_PATH, "wb") as AP:
         for chunk in link.iter_content(100000):
-            TP.write(chunk)
+            AP.write(chunk)
 
 def unpack_datas():
-
+    """Function unzip archive with datas
+    """
     packed_datas = zipfile.ZipFile(ARCHIVE_PATH)
 
     for file in packed_datas.namelist():
