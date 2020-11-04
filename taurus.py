794b32b868365ae373d3cb78cc90b8843594c961
heniu1985 <heniu_1@o2.pl>
2020-11-04 14:03:13 +0100

    Uncomment taurus.py

--- taurus.py
+++ taurus.py
@@ -236,24 +236,24 @@ def close_short_position(df):
 
 def main():
 
-    # try:
-    #     """Data download"""
+    try:
+        """Data download"""
 
-    #     download_datas()
-    #     unpack_datas()
-    #     change_extensions()
+        download_datas()
+        unpack_datas()
+        change_extensions()
 
-    #     """Data format"""
+        """Data format"""
 
-    #     for path in paths_to_file():
-    #         ftdl = file_to_dicts_lists(path)
-    #         duk = del_unnecessary_keys(ftdl)
-    #         cddf = change_dicts_dates_format(duk)
-    #         back_to_file(path, cddf)
+        for path in paths_to_file():
+            ftdl = file_to_dicts_lists(path)
+            duk = del_unnecessary_keys(ftdl)
+            cddf = change_dicts_dates_format(duk)
+            back_to_file(path, cddf)
         
-    #     emails.files_downloaded()
-    # except:
-    #     emails.download_error()
+        emails.files_downloaded()
+    except:
+        emails.download_error()
 
     """Signals countig"""
 
